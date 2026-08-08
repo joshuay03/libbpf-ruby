e0e5fed18d0dad65e4fcffa35e1dcfa1d0415936
Joshua Young <djry1999@gmail.com>
2026-08-09 09:30:13 +1000

    Add a Ractor safety test
    The extension is already flagged with rb_ext_ractor_safe. Cover it
    with a smoke test that loads an object, resolves a program, and
    closes the object all inside a Ractor.

--- test/libbpf-ruby/test_libbpf_ruby.rb
+++ test/libbpf-ruby/test_libbpf_ruby.rb
@@ -6,4 +6,14 @@ class TestLibBPFRuby < Minitest::Test
   def test_version
     refute_nil LibBPFRuby::VERSION
   end
+
+  def test_ractor_safe
+    ractor = Ractor.new(BPF_OBJECT_PATH) do |path|
+      object = LibBPFRuby::Object.new(path)
+      fd = object.program_fd("test_program")
+      object.close
+      fd
+    end
+    assert_kind_of Integer, (ractor.respond_to?(:value) ? ractor.value : ractor.take)
+  end
 end
